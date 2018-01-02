f3c3a415bf511084b56c84108bec93870e27bf27
khanh-nuarxinc <khanh@nuarxinc.com>
2018-01-02 13:39:09 -0500

    refactor

--- AzureFunctionTest/HelloWorldHttpTrigger.cs
+++ AzureFunctionTest/HelloWorldHttpTrigger.cs
@@ -30,7 +30,7 @@ namespace AzureFunctionTest
             name = name ?? data?.name;
 
             return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass anything in the request body")
                 : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
         }
     }
